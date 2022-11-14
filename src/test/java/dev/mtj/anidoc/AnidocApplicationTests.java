11a78b245e6e9889396bc2b096047cf4418a16d3
JamesLeMaitre <jamestchao14@gmail.com>
2022-11-14 10:28:05 +0000

    first commit

---
+++ src/test/java/dev/mtj/anidoc/AnidocApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mtj.anidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnidocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
